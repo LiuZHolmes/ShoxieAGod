f00c8f7336241129eb886b177aa1a1a391c5c684
retr0 <395637649@qq.com>
2019-10-15 19:49:33 +0800

    extract method

--- plugins/5e_query.py
+++ plugins/5e_query.py
@@ -5,11 +5,8 @@ from nonebot import on_command, CommandSession
 async def _5e(session: CommandSession):
     user_name = session.get('user_name')
     history = await get_recent_history_of_user(user_name)
-    result = ''
-    for i in history:
-        result += '胜' if i['is_win'] == '1' else '负'
-        pass
-    await session.send(f'{user_name}的战绩是{result}')
+    result = build_recent_history_result(history)
+    await session.send(f'{user_name}的最近战绩是{result}')
 
 
 @_5e.args_parser
@@ -24,8 +21,8 @@ async def _(session: CommandSession):
 async def recent(session: CommandSession):
     user_name = session.get('user_name')
     history = await get_recent_history_of_user(user_name)
-    match = history[0]
-    await session.send(f'玩家：{user_name}\n比赛时间：{match["round_time"]}\n地图：{match["map"]}\n击杀数：{match["kill"]}\nrating：{match["rating"]}')
+    result = build_recent_match_statistic(history, user_name)
+    await session.send(result)
 
 
 @recent.args_parser
@@ -44,3 +41,15 @@ async def get_recent_history_of_user(user_name: str) -> str:
     dataObj = json.loads(data)
     history = dataObj['data']
     return history
+
+
+def build_recent_history_result(history):
+    result = ''
+    for i in history:
+        result += '胜' if i['is_win'] == '1' else '负'
+        pass
+    return result
+
+def build_recent_match_statistic(history, user_name):
+    match = history[0]
+    return f'玩家：{user_name}\n比赛时间：{match["round_time"]}\n地图：{match["map"]}\n击杀数：{match["kill"]}\nrating：{match["rating"]}'
