e16ed57e09b87a32f14d22c3c59ae12bfcf25e64
retr0 <395637649@qq.com>
2019-10-19 10:23:18 +0800

    add changelog

---
+++ plugins/common.py
@@ -0,0 +1,11 @@
+from nonebot import on_command, CommandSession
+
+
+@on_command('changelog')
+async def changelog(session: CommandSession):
+    with open('resource/changelog.txt', 'r', encoding='utf-8') as f:
+        try:
+            str = f.read()
+        except:
+            f.close()
+    await session.send(str)
