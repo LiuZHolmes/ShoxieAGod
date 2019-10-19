ba0c2083795188e7241e8c96d8dd6dc58420676b
retr0 <395637649@qq.com>
2019-10-19 10:45:37 +0800

    modify changelog path

--- plugins/common.py
+++ plugins/common.py
@@ -3,7 +3,7 @@ from nonebot import on_command, CommandSession
 
 @on_command('changelog')
 async def changelog(session: CommandSession):
-    with open('resource/changelog', 'r', encoding='utf-8') as f:
+    with open('./resource/changelog', 'r', encoding='utf-8') as f:
         try:
             str = f.read()
         except:
