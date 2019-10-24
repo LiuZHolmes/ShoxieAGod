1bb6432d80c70dd446dcd8ee11f884c8c3a6f380
retr0 <395637649@qq.com>
2019-10-24 19:49:52 +0800

    feat: whois command

---
+++ plugins/liquipedia_query.py
@@ -0,0 +1,19 @@
+from liquipediapy import counterstrike
+from nonebot import on_command, CommandSession
+
+counterstrike_obj = counterstrike("ShoxieAGod")
+
+
+@on_command('whois')
+async def who_is(session: CommandSession):
+    user_name = session.get('player')
+    player_details = counterstrike_obj.get_player_info(user_name, True)
+    await session.send(player_details)
+
+
+@who_is.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if stripped_arg:
+        session.state['player'] = stripped_arg
+    return
