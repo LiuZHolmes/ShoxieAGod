9f25a3b01975974559a5edf5fc3108a882346352
retr0 <395637649@qq.com>
2019-10-30 19:50:33 +0800

    feat: upcoming command

--- plugins/liquipedia_query.py
+++ plugins/liquipedia_query.py
@@ -21,6 +21,21 @@ async def _(session: CommandSession):
     return
 
 
+@on_command('upcoming')
+async def upcoming(session: CommandSession):
+    team = session.get('team')
+    result = get_upcoming_by_team(team)
+    await session.send(result)
+
+
+@upcoming.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if stripped_arg:
+        session.state['team'] = stripped_arg
+    return
+
+
 def build_pro_player_detail(player):
     player_detail = counterstrike_obj.get_player_info(player, True)
     result = extract_field_by_attribute(player_detail["info"], '姓名', 'name')
@@ -31,3 +46,19 @@ def build_pro_player_detail(player):
     result += extract_field_by_attribute(player_detail["info"], '队伍', 'team')
     result += extract_field_by_attribute(player_detail["info"], '曾用ID', 'alternate_ids', True)
     return result
+
+
+def get_upcoming_by_team(team):
+    games = counterstrike_obj.get_upcoming_and_ongoing_games()
+    match = find_upcoming_match_by_team(games, team)
+    return build_match(match)
+
+
+def find_upcoming_match_by_team(games, team):
+    return next(filter(lambda x: x['team1'] == team or x['team2'] == team, games))
+
+
+def build_match(match):
+    return f"{match['team1']} VS {match['team2']}\n" \
+           f"开始时间：{match['start_time']}\n" \
+           f"所属赛事：{match['tournament']}"
